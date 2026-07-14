9d36b430720feff943a8be5200f2677042136064
thetnaung-dev <thet123990@gmail.com>
2026-07-14 14:39:43 +0630

    fix: use size state instead of sizes data in toast title

--- app/(tabs)/(home)/detail.tsx
+++ app/(tabs)/(home)/detail.tsx
@@ -4,6 +4,7 @@ import { ScrollView } from "react-native";
 
 import Cart from "@/components/shop/cart";
 import ViewPager from "@/components/shop/viewpager";
+import { Button, ButtonText } from "@/components/ui/button";
 import {
   Checkbox,
   CheckboxGroup,
@@ -15,16 +16,48 @@ import { HStack } from "@/components/ui/hstack";
 import { CheckIcon, FavouriteIcon, Icon, StarIcon } from "@/components/ui/icon";
 import { Pressable } from "@/components/ui/pressable";
 import { Text } from "@/components/ui/text";
+import {
+  Toast,
+  ToastDescription,
+  ToastTitle,
+  useToast,
+} from "@/components/ui/toast";
 import { VStack } from "@/components/ui/vstack";
-import { products } from "@/data";
+import { products, sizes } from "@/data";
 
 const Detail = () => {
   const { id } = useLocalSearchParams();
   const [more, setMore] = useState(false);
-  const [colors, setColors] = useState(false);
-  const [size, setSizes] = useState(false);
-  const [values, setValues] = useState(["Illustration"]);
+  const [colors, setColors] = useState([]);
+  const [size, setSizes] = useState([]);
   const product = products.find((p) => p.id === +id);
+
+  const toast = useToast();
+  const [toastId, setToastId] = useState(0);
+  const handleToast = (title: string, description: string) => {
+    if (!toast.isActive(toastId.toString())) {
+      showNewToast(title, description);
+    }
+  };
+  const showNewToast = (title: string, description: string) => {
+    const newId = Math.random();
+    setToastId(newId);
+    toast.show({
+      id: newId.toString(),
+      placement: "bottom",
+      duration: 2000,
+      render: ({ id }) => {
+        const uniqueToastId = "toast-" + id;
+        return (
+          <Toast nativeID={uniqueToastId} action="info" variant="solid">
+            <ToastTitle>{title}</ToastTitle>
+            <ToastDescription>{description}</ToastDescription>
+          </Toast>
+        );
+      },
+    });
+  };
+
   return (
     <VStack className="flex-1 bg-white">
       <Stack.Screen
@@ -76,32 +109,62 @@ const Detail = () => {
           </VStack>
           <Text className="mt-2 mb-1">Choose Colors</Text>
           <CheckboxGroup
-            value={values}
+            value={colors}
             onChange={(keys) => {
-              setValues(keys);
+              setColors(keys);
             }}
           >
-            <HStack space="2xl">
-              <Checkbox value="Illustration">
-                <CheckboxIndicator>
-                  <CheckboxIcon as={CheckIcon} />
-                </CheckboxIndicator>
-                <CheckboxLabel>Illustration</CheckboxLabel>
-              </Checkbox>
-              <Checkbox value="Animation">
-                <CheckboxIndicator>
-                  <CheckboxIcon as={CheckIcon} />
-                </CheckboxIndicator>
-                <CheckboxLabel>Animation</CheckboxLabel>
-              </Checkbox>
-              <Checkbox value="Typography">
-                <CheckboxIndicator>
-                  <CheckboxIcon as={CheckIcon} />
-                </CheckboxIndicator>
-                <CheckboxLabel>Typography</CheckboxLabel>
-              </Checkbox>
+            <HStack space="xl">
+              {product?.colors.map((item) => (
+                <Checkbox
+                  value={item.name}
+                  key={item.id}
+                  isDisabled={!item.stock}
+                >
+                  <CheckboxIndicator>
+                    <CheckboxIcon as={CheckIcon} />
+                  </CheckboxIndicator>
+                  <CheckboxLabel>{item.name}</CheckboxLabel>
+                </Checkbox>
+              ))}
             </HStack>
           </CheckboxGroup>
+          <Text className="mt-2 mb-1">Choose Sizes</Text>
+          <CheckboxGroup
+            value={size}
+            onChange={(keys) => {
+              setSizes(keys);
+            }}
+          >
+            <HStack space="xl">
+              {product?.sizes.map((item) => (
+                <Checkbox
+                  value={item.name}
+                  key={item.id}
+                  isDisabled={!item.stock}
+                >
+                  <CheckboxIndicator>
+                    <CheckboxIcon as={CheckIcon} />
+                  </CheckboxIndicator>
+                  <CheckboxLabel>{item.name}</CheckboxLabel>
+                </Checkbox>
+              ))}
+            </HStack>
+          </CheckboxGroup>
+          <Button
+            size="lg"
+            className="mt-6 self-start bg-sky-500"
+            onPress={() => {
+              if (colors.length > 0 && size.length > 0) {
+                return;
+              }
+              const title = `Must choose ${colors.length === 0 ? "color" : ""}${colors.length === 0 && size.length === 0 ? " - " : ""}${size.length === 0 ? "size" : ""}`;
+              const description = "Please set quantity just after choosing ";
+              handleToast(title, description);
+            }}
+          >
+            <ButtonText>Set Quatity</ButtonText>
+          </Button>
         </VStack>
       </ScrollView>
     </VStack>
